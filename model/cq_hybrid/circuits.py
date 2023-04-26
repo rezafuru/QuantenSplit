60ae9b686f34033f2a91412ac4d29725ba2ac3b3
Felix Truger <17719187+FelixTruger@users.noreply.github.com>
2023-04-26 21:33:13 +0200

    Add alternating rotation circuits

--- model/cq_hybrid/circuits.py
+++ model/cq_hybrid/circuits.py
@@ -34,3 +34,66 @@ def mari_quantum_circuit(q_input_features, q_weights_flat):
 
     exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(qubits)]
     return tuple(exp_vals)
+
+
+@qml.qnode(dev, interface="torch")
+def alternating_rotation_circuit(q_input_features, q_weights_flat):
+    """
+        The variational quantum circuit used in the Paper
+    """
+
+    q_weights = q_weights_flat.reshape(q_depth, qubits)
+
+    H_layer(qubits)
+
+    RZ_layer(q_input_features)
+
+    # Sequence of parameterized layers with alternating rotations
+    for k in range(q_depth):
+        entangling_layer(qubits)
+        if k % 2 == 0:
+            RY_layer(q_weights[k])
+        else:
+            RZ_layer(q_weights[k])
+
+    exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(qubits)]
+    return tuple(exp_vals)
+
+
+@register_q_circuit
+@qml.qnode(dev, interface="torch")
+def alternating_rotation_circuit_a1(q_input_features, q_weights_flat):
+
+    q_weights = q_weights_flat.reshape(q_depth, qubits)
+
+    RY_layer(q_input_features)
+
+    for k in range(q_depth):
+        entangling_layer_linear(qubits)
+        if k % 2 == 0:
+            RZ_layer(q_weights[k])
+        else:
+            RY_layer(q_weights[k])
+
+    exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(qubits)]
+    return tuple(exp_vals)
+
+
+
+@register_q_circuit
+@qml.qnode(dev, interface="torch")
+def alternating_rotation_circuit_a2(q_input_features, q_weights_flat):
+    q_weights = q_weights_flat.reshape(q_depth, qubits)
+
+
+    RY_layer(q_input_features)
+
+    for k in range(q_depth):
+        entangling_layer(qubits)
+        if k % 2 == 0:
+            RZ_layer(q_weights[k])
+        else:
+            RY_layer(q_weights[k])
+
+    exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(qubits)]
+    return tuple(exp_vals)
